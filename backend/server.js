8fbbd2853f24342c10e8585db521c72eb23ae653
Deepak <deepsk3697@gmail.com>
2021-11-18 00:20:34 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,40 @@
 const express = require("express")
 const notes = require('./data/notes')
 const dotenv = require('dotenv')
+const connectDB = require("./config/db")
+const userRoutes  = require('./routes/userRoutes.js')
+const noteRoutes  = require('./routes/noteRoutes.js')
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware")
+const path = require("path")
 
 
 const app = express()
 dotenv.config()
+connectDB()
+app.use(express.json())
 
-app.get("/",(req,res) => {
-    res.send("Hello from server")
-})
 
-app.get("/api/notes",(req,res)=>{
-    res.json(notes)
-})
+app.use('/api/users',userRoutes)
+app.use('/api/notes',noteRoutes)
 
-app.get("/api/notes/:id",(req,res) => {
-    const note = notes.find((n) => n._id === req.params.id)
-    res.json(note)
-})
+// ------------------------DEPLOYMENT---------------------
+
+__dirname=path.resolve()
+if(process.env.NODE_ENV === 'production'){
+ app.use(express.static(path.join(__dirname,'/frontend/build')))
+
+ app.get('*', (req,res) => {
+  res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+ })
+} else {
+    app.get("/",(req,res) => {
+        res.send("Hello from server")
+    })
+}
+
+
+// ------------------------DEPLOYMENT---------------------
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(process.env.PORT,console.log("server started"))
